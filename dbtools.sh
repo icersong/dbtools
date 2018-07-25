74cdff1e03e9deb7d7530306f32bb5a45bae6912
icersong <icersong@gmail.com>
2018-07-25 20:28:24 +0800

    Refactory v2.0

--- dbtools.sh
+++ dbtools.sh
@@ -2,7 +2,7 @@
 # Copyright: Wed Jan 14 20:17:38 CST 2015
 # Author: icersong
 # Modified: Thu Jan 15 03:47:03 CST 2015
-# Version: 1.1
+# Version: 2.0
 
 
 ################################################################################
@@ -16,6 +16,22 @@ command -v pv >/dev/null 2>&1 || { echo >&2 "Error! require pv but it's not inst
 command -v tar >/dev/null 2>&1 || { echo >&2 "Error! require tar but it's not installed.  Abort."; exit 1; }
 command -v 7za >/dev/null 2>&1 || { echo >&2 "Warning! require 7za but it's not installed."; }
 command -v unzip >/dev/null 2>&1 || { echo >&2 "Warning! require unzip but it's not installed."; }
+
+function check_command_error() {
+    command -v $1 >/dev/null 2>&1 || {
+        echo >&2 "Error! require command $1 but it's not installed.  Abort!!!";
+        exit 1;
+    }
+}
+
+function check_command_warn() {
+    check_command_warn=1
+    command -v $1 >/dev/null 2>&1 || {
+        echo >&2 "Warning! require command $1 but it's not installed.";
+        check_command_warn=0;
+    }
+}
+
 
 ################################################################################
 # funciton selectfile   {{{1
@@ -139,103 +155,159 @@ scriptpath=$(cd `dirname $0`; pwd)
 action="$1"
 if [ -z "${action}" ]; then
     echo "Usage:"
-    echo "\$ ${scriptfile} <action> [dbname [sqlfile]]"
-    echo "  action: backup|import|restore|create|remove"
+    echo "\$ ${scriptfile} <create|remove|backup|import|export|restore> [dbname ...]"
+    echo "\$ ${scriptfile} create [dbname]"
+    echo "\$ ${scriptfile} remove [dbname]"
+    echo "\$ ${scriptfile} backup [dbname]"
+    echo "\$ ${scriptfile} import [dbname [filename]]"
+    echo "\$ ${scriptfile} export [dbname[:tables] [filename]]"
+    echo "\$ ${scriptfile} restore [dbname [filename]]"
     exit;
 fi
 
 
 ################################################################################
 # config variables
-dbuser=
-dbpass=
-dbname=
-dbhost=
-dbport=
-dumpargs=
-dumpexec=
-sqlfile=
-sqlpath=
-
-
-################################################################################
-# script config     {{{1
-
 scriptconfig="$scriptpath/${scriptname}.cfg"
-echo scriptconf: $scriptconfig
 if [ ! -f "$scriptconfig" ]; then
     echo "Error! Cofnig file '$scriptconfig' not exists."
     exit
 fi
-for line in  `cat $scriptconfig`
-do
-    if [ "$line" == "" ]; then
-        continue
-    fi
-    name=${line%=*}
-    text=${line##*=}
-    case "$name" in
-        "database.user")
-            dbuser=${text}
-            ;;
-        "database.pass")
-            dbpass=${text}
-            ;;
-        "database.name")
-            dbname=${text}
-            ;;
-        "database.host")
-            dbhost=${text}
-            ;;
-        "database.port")
-            dbport=${text}
-            ;;
-        "dump.args")
-            dumpargs=${text}
-            ;;
-        "dump.exec")
-            dumpexec=${text}
-            ;;
-        "sql.path")
-            sqlpath=${text}
-            ;;
-        "*") ;;
-    esac
-done
 
-if [ -z "${sqlpath}" ]; then
-    sqlpath="."
+scriptname=${scriptfile%.*}
+script_ext=${scriptfile##*.}
+
+dbname="${2%:*}"
+if [ "$dbname" != "$2" ]; then
+    tables=${2##*.}
 fi
-
-echo "action: ${action}"
-echo "database.user: ${dbuser}"
-echo "database.pass: ${dbpass}"
-echo "database.name: ${dbname}"
-echo "database.host: ${dbhost}"
-echo "database.port: ${dbport}"
-echo "dump.args: ${dumpargs}"
-echo "dump.exec: ${dumpexec}"
-echo "sql.path: ${sqlpath}"
-echo "sql.file: ${sqlfile}"
-
+filename="$3"
+dumpargs=
+dumpexec=
+charset="charset utf8 COLLATE utf8_general_ci"
 dbexec="mysql"
-dbargs="-u${dbuser} -p${dbpass} --host ${dbhost} --port ${dbport}"
-dbargs=" --defaults-extra-file=${scriptpath}/mysql-extra-config.cnf"
+dbargs="--defaults-extra-file=${scriptconfig}"
 dbwarn="^Warning: Using a password on the command line interface can be insecure."
 
-
-################################################################################
-# Input args {{{1
-if [ -z "${dbname}" ] && [ "$2" != "" ]; then
-    dbname="$2"
-fi
-if [ -z "${sqlfile}" ] && [ "$3" != "" ]; then
-    sqlfile="$3"
-fi
+# echo "action: ${action}"
+# echo "database: ${dbname}"
+# echo "filename: ${filename}"
+# echo "dump.args: ${dumpargs}"
+# echo "dump.exec: ${dumpexec}"
+# echo scriptconf: $scriptconfig
 
 
 ################################################################################
 # do command    {{{1
+
+function select_sql_file () {
+    # selected sql file
+    if [ -z "${filename}" ] || [ ! -f "${filename}" ]; then
+        selectfile "*.sql *.zip *.7z *.tgz *.tar.gz";
+        if [ ! -f "${selected}" ]; then
+            echo "No sql file selected."
+            exit;
+        fi
+        filename=${selected}
+    fi
+    echo "Sql file $filename selected."
+}
+
+function select_database() {
+    if [ -z "${dbname}" ]; then
+        get_dbname "${dbexec} ${dbargs}"
+    fi
+}
+
+function action_import () {
+    # import database
+    echo "Import data from ${filename}"
+    check_command_error 'pv';
+    execstr="${dbexec} ${dbargs} --database ${dbname}"
+    if [ "${filename##*.tar.}" == "gz" ] || [ "${filename##*.}" == "tgz" ]; then
+        check_command_error 'tar';
+        tarfile=`tar -tf ${filename} | grep -v "/"`
+        pv ${filename}|tar -zxO ${tarfile}|${execstr} 2>&1|grep -v "${dbwarn}"
+    elif [ "${filename##*.}" == "zip" ]; then
+        check_command_error 'unzip';
+        command -v unzip >/dev/null 2>&1 || {
+            echo >&2 "I Error! require unzip but it's not installed.  Aborting."; exit 1;
+        }
+        size=`unzip -l ${filename}|tail -n1|awk '{print $1}'`
+        unzip -p ${filename}|pv -s ${size}|${execstr} 2>&1|grep -v "${dbwarn}"
+    elif [ "${filename##*.}" == "7z" ]; then
+        check_command_error '7za';
+        if [ -n `which 7za` ]; then
+            size=`7za l ${filename}|grep ".sql"|awk '{print $4}'`
+            7za x -so ${filename} 2>/dev/null|pv -s ${size}|${execstr} 2>&1|grep -v "${dbwarn}"
+        else
+            echo "Error! Command 7za not found. Please do command brew install p7zip."
+            exit;
+        fi
+    elif [ "${filename##*.}" == "sql" ]; then
+        pv ${filename}|${execstr} 2>&1|grep -v "${dbwarn}"
+    else
+        echo "Warning! Unknown selected file type. ${filename}"
+    fi
+}
+
+# $1: full-filename
+# $2: compress name with out ext
+action_compress () {
+    lst='tar zip'
+    for x in $lst
+    do
+        check_command_warn "$x";
+        if [ "$check_command_warn" == "1" ]; then
+            case "${x}" in
+                "tar")
+                    cmp="tar zvcf"
+                    ext=".tar.gz"
+                    ;;
+                "*")
+                    cmp="$x"
+                    ext=".$x"
+            esac
+            break;
+        fi
+    done
+    if [ "$cmp" == "" ]; then
+        echo "No compression tools. ignore compress."
+        echo "Rename file to ${2}${ext}"
+
+    else
+        echo "${cmp} -> ${2}${ext}"
+        ${cmp} ${2}${ext} ${1} >/dev/null
+    fi
+}
+
+
+# $1: dbname
+# $2: tables
+# $3: filename
+action_dumpdata () {
+    if [ "$2" == "" ]; then
+        mysqldump ${dbargs} ${dumpargs} ${1} 2>/dev/null| pv > ${3}
+    else
+        mysqldump ${dbargs} ${dumpargs} -B ${1} --tables ${2} 2>/dev/null| pv > ${3}
+    fi
+}
+
+
+action_export () {
+    select_database;
+    echo "Dump data -> ~.sql"
+    action_dumpdata "${dbname}" "${tables}" "~.sql";
+    name=`date "+%Y%m%d%H%M%S"`
+    echo "Move ~.sql -> ${name}.sql"
+    mv ~.sql ${name}.sql
+    if [ "${filename}" == "" ]; then
+        filename="${dbname}-${name}"
+    fi
+    action_compress ${name}.sql ${filename};
+    rm -f ${name}.sql ~.sql
+}
+
 
 case "${action}" in
     "create")
@@ -246,87 +318,33 @@ case "${action}" in
             echo "No database specified."
             exit;
         fi
-        ${dbexec} ${dbargs} -e "CREATE DATABASE IF NOT EXISTS ${dbname} default charset utf8 COLLATE utf8_general_ci;"
+        ${dbexec} ${dbargs} -e "CREATE DATABASE IF NOT EXISTS ${dbname} default ${charset};"
         echo "Database '${dbname}' created!"
         ;;
     "remove")
-        if [ -z "${dbname}" ]; then
-            get_dbname "${dbexec} ${dbargs}"
-        fi
+        select_database;
         ${dbexec} ${dbargs} -e "DROP DATABASE IF EXISTS ${dbname};"
         echo "Database '${dbname}' removed!"
         ;;
     "backup")
-        if [ -z "${dbname}" ]; then
-            get_dbname "${dbexec} ${dbargs}"
-        fi
-        echo "Backup data to ~.sql"
-        mysqldump ${dbargs} ${dumpargs} ${dbname} 2>/dev/null| pv > ~.sql
-        name=`date "+%Y%m%d%H%M%S"`
-        echo "Move ~.sql -> ${name}.sql"
-        mv ~.sql ${name}.sql
-        echo "Make zip file ${dbname}-${name}.zip"
-        zip ${dbname}-${name}.zip ${name}.sql >/dev/null
-        rm -f ${name}.sql ~.sql
-        echo 'complete!!!'
-        exit;
+        action_export;
+        ;;
+    "export")
+        action_export;
         ;;
     "import")
         # selected sql file
-        if [ -z "${sqlfile}" ] || [ ! -f "${sqlfile}" ]; then
-            selectfile "${sqlpath}/*.sql ${sqlpath}/*.zip ${sqlpath}/*.7z ${sqlpath}/*.tar.gz";
-            if [ ! -f "${selected}" ]; then
-                echo "No sql file selected."
-                exit;
-            fi
-            sqlfile=${selected}
-        fi
-        echo "Sql file $sqlfile selected."
+        select_sql_file;
         # select database
-        if [ -z "${dbname}" ]; then
-            get_dbname "${dbexec} ${dbargs}"
-        fi
-        # import database
-        echo "Import data from ${sqlfile}"
-        execstr="${dbexec} ${dbargs} --database ${dbname}"
-        if [ "${sqlfile##*.tar.}" == "gz" ] || [ "${sqlfile##*.}" == "tgz" ]; then
-            tarfile=`tar -tf ${sqlfile} | grep -v "/"`
-            pv ${sqlfile}|tar -zxO ${tarfile}|${execstr} 2>&1|grep -v "${dbwarn}"
-        elif [ "${sqlfile##*.}" == "zip" ]; then
-            command -v unzip >/dev/null 2>&1 || { echo >&2 "I Error! require unzip but it's not installed.  Aborting."; exit 1; }
-            size=`unzip -l ${sqlfile}|tail -n1|awk '{print $1}'`
-            unzip -p ${sqlfile}|pv -s ${size}|${execstr} 2>&1|grep -v "${dbwarn}"
-        elif [ "${sqlfile##*.}" == "7z" ]; then
-            if [ -n `which 7za` ]; then
-                size=`7za l ${sqlfile}|grep ".sql"|awk '{print $4}'`
-                7za x -so ${sqlfile} 2>/dev/null|pv -s ${size}|${execstr} 2>&1|grep -v "${dbwarn}"
-            else
-                echo "Error! Command 7za not found. Please do command brew install p7zip."
-                exit;
-            fi
-        elif [ "${sqlfile##*.}" == "sql" ]; then
-            pv ${sqlfile}|${execstr} 2>&1|grep -v "${dbwarn}"
-        else
-            echo "Unknown selected file type. ${sqlfile}"
-        fi
-        echo "completed!!!"
-        exit;
+        select_database;
+        # do action
+        action_import;
         ;;
     "restore")
         # selected sql file
-        if [ -z "${sqlfile}" ] || [ ! -f "${sqlfile}" ]; then
-            selectfile "${sqlpath}/*.sql ${sqlpath}/*.zip ${sqlpath}/*.7z ${sqlpath}/*.tar.gz";
-            if [ ! -f "${selected}" ]; then
-                echo "No sql file selected."
-                exit;
-            fi
-            sqlfile=${selected}
-        fi
-        echo "Sql file $sqlfile selected."
+        select_sql_file;
         # select database
-        if [ -z "${dbname}" ]; then
-            get_dbname "${dbexec} ${dbargs}"
-        fi
+        select_database;
         # drop database
         echo "Drop database ${dbname}"
         # echo "drop database if exists ${dbname};"|${dbexec} ${dbargs} 2>&1|grep -v "${dbwarn}"
@@ -335,33 +353,9 @@ case "${action}" in
         echo "Create database ${dbname}"
         echo "create database ${dbname};"|${dbexec} ${dbargs} 2>&1|grep -v "${dbwarn}"
         # import database
-        echo "Import data from ${sqlfile}"
-        execstr="${dbexec} ${dbargs} --database ${dbname}"
-        if [ "${sqlfile##*.tar.}" == "gz" ] || [ "${sqlfile##*.}" == "tgz" ]; then
-            tarfile=`tar -tf ${sqlfile}|grep -v "/"`
-            pv ${sqlfile}|tar -zxO ${tarfile}|${execstr} 2>&1|grep -v "${dbwarn}"
-        elif [ "${sqlfile##*.}" == "zip" ]; then
-            command -v unzip >/dev/null 2>&1 || { echo >&2 "I Error! require unzip but it's not installed.  Aborting."; exit 1; }
-            size=`unzip -l ${sqlfile}|tail -n1|awk '{print $1}'`
-            unzip -p ${sqlfile}|pv -s ${size}|${execstr} 2>&1|grep -v "${dbwarn}"
-        elif [ "${sqlfile##*.}" == "7z" ]; then
-            if [ -n `which 7za` ]; then
-                size=`7za l ${sqlfile}|grep ".sql"|awk '{print $4}'`
-                7za x -so ${sqlfile} 2>/dev/null|pv -s ${size}|${execstr} 2>&1|grep -v "${dbwarn}"
-            else
-                echo "Error! Command 7za not found. Please do command brew install p7zip."
-                exit;
-            fi
-        elif [ "${sqlfile##*.}" == "sql" ]; then
-            pv ${sqlfile}|${execstr} 2>&1|grep -v "${dbwarn}"
-        else
-            echo "Unknown selected file type. ${sqlfile}"
-        fi
-        echo "completed!!!"
-        exit;
+        action_import;
         ;;
     "*")
         echo "Unknown action ${action}!!!"
-        exit;
         ;;
 esac
